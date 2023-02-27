d64a127c7b7dc5e95c5b521180947fa1a88af487
Stefano Gerace <sgerace@e4r3p2.42roma.it>
2023-02-27 19:14:44 +0100

    gli args vengono caricati creando una matrice con elementi le stringhe inserite dall utente, il primo elemento che dovrebbe essere il comando viene caricato con la path corrente usando la funzione ft_trypath, mentre le redirection finali vengono eliminate e al loro posto viene messo NULL, per soddisfare le richieste dell execve

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: sgerace <sgerace@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 19:31:05 by sgerace           #+#    #+#             */
-/*   Updated: 2023/02/27 18:17:24 by sgerace          ###   ########.fr       */
+/*   Updated: 2023/02/27 19:12:51 by sgerace          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -167,17 +167,48 @@ char	*ft_trypath(char	*cmd, t_list **envp)
 	return (NULL);
 }
 
+char	**ft_load_args(t_list **cmd_list, t_list **envp)
+{
+	int		i;
+	char 	**args;
+	t_list	*head;
+
+	i = 0;
+	head = *cmd_list;
+	while (head->cmd_m[i])
+	{
+		i++;
+	}
+	args = (char **) malloc (sizeof(char *) * (i + 1));
+	head->cmd_m[0] = ft_trypath(head->cmd_m[0], envp);
+	i = 0;
+	while (head->cmd_m[i])
+	{
+		args[i] = (char *) malloc (sizeof(char) * ft_strlen(head->cmd_m[i]) + 1);
+		args[i] = head->cmd_m[i];
+		i++;
+	}
+	if (args[i - 1][ft_strlen(head->cmd_m[i - 1]) - 1] == '>' || args[i - 1][ft_strlen(head->cmd_m[i - 1]) - 1] == '<' || args[i - 1][ft_strlen(head->cmd_m[i - 1]) - 1] == '|')
+	{
+		ft_printf("Sostituito redirection with NULL\n");
+		args[i - 1] = NULL;
+	}
+	return (args);
+}
+
 int ft_start_executing(t_list	**cmd_list, int cmd_num, t_list **envp)
 {
 	t_list		*head;
 	int			*pid;
 	int			i;
 	int 		**pipes;
-	char 		*trypath;
+	char		**args;
 
 	pid = (int*) malloc (sizeof(int) * cmd_num);
 	pipes = (int**) malloc (sizeof(int) * cmd_num + 1);
+	
 
+	args = NULL;
 	head = *cmd_list;
 
 	i = 0;
@@ -195,18 +226,7 @@ int ft_start_executing(t_list	**cmd_list, int cmd_num, t_list **envp)
 	i = 0;
 	while(head)
 	{
-		trypath = ft_trypath(head->cmd_m[0], envp);
-		if(trypath == NULL)
-		{
-			ft_printf("Command not found/mispelled\n");
-		}
-		else
-		{
-			head->cmd_m[0] = trypath;
-			// ft_printf("%sZ\n", head->cmd_m[0]);
-		}
-
-		char *args[] = {trypath, head->cmd_m[1], NULL}; //da mettere in un while per unire tutti i vari argomenti in args
+		args = ft_load_args(&head, envp);
 
 		pid[i] = fork();
 		if (pid[i] < 0)
